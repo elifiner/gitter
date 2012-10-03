46bcef7d2c7cd84bb224ca0ad17952a7fe25abf0
Eli Finer <elig@il.ibm.com>
2012-10-03 02:02:33 +0200

    fixed bash completion to work with ubuntu git package

--- gitter-completion.bash
+++ gitter-completion.bash
@@ -5,7 +5,8 @@
 # Copyright (C) 2012 Eli Finer <eli.finer@gmail.com>
 # Uses and depends on git-completion.bash from git/contrib/completion.
 #
-source /etc/bash_completion.d/git-completion.bash
+source /etc/bash_completion.d/git 2> /dev/null
+source /etc/bash_completion.d/git-completion.bash 2> /dev/null
 complete -o bashdefault -o default -o nospace -F _git gt 2>/dev/null \
 	|| complete -o default -o nospace -F _git gt
 complete -o bashdefault -o default -o nospace -F _git gitter 2>/dev/null \
