d052a0b2dc2437cbf6d74265952df777b61040f5
Eli Finer <eli.finer@gmail.com>
2016-09-01 20:37:01 -0700

    fixed open file warnings for tests

--- test.py
+++ test.py
@@ -25,38 +25,38 @@ class GitterTests(unittest.TestCase):
     def test_help(self):
         gt = TestingGitRunner(["--help"])
         gt.run()
-        self.assertEquals(GIT + "--help", gt._last_command)
+        self.assertEqual(GIT + "--help", gt._last_command)
 
 class GitterTestCheckout(unittest.TestCase):
     def test_checkout_local_branch(self):
         gt = TestingGitRunner(["checkout"])
         gt._selected = [gitter.LocalBranch('develop')]
         gt.run()
-        self.assertEquals(GIT + "checkout develop", gt._last_command)
+        self.assertEqual(GIT + "checkout develop", gt._last_command)
 
     def test_remote_branch(self):
         gt = TestingGitRunner(["checkout"])
         gt._selected = [gitter.RemoteBranch('bugfix')]
         gt.run()
-        self.assertEquals(GIT + "checkout -b bugfix bugfix", gt._last_command)
+        self.assertEqual(GIT + "checkout -b bugfix bugfix", gt._last_command)
 
     def test_modified(self):
         gt = TestingGitRunner(["checkout"])
         gt._selected = [gitter.ModifiedFile('somefile.py')]
         gt.run()
-        self.assertEquals(GIT + "checkout somefile.py", gt._last_command)
+        self.assertEqual(GIT + "checkout somefile.py", gt._last_command)
 
     def test_deleted(self):
         gt = TestingGitRunner(["checkout"])
         gt._selected = [gitter.DeletedFile('deleted.py')]
         gt.run()
-        self.assertEquals(GIT + "checkout deleted.py", gt._last_command)
+        self.assertEqual(GIT + "checkout deleted.py", gt._last_command)
 
     def test_multiple(self):
         gt = TestingGitRunner(["checkout"])
         gt._selected = [gitter.ModifiedFile('somefile.py'), gitter.DeletedFile('deleted.py')]
         gt.run()
-        self.assertEquals(GIT + "checkout somefile.py deleted.py", gt._last_command)
+        self.assertEqual(GIT + "checkout somefile.py deleted.py", gt._last_command)
 
 if __name__ == '__main__':
     unittest.main()
